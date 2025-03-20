1206542dff843e3ce867f3e3b6441c12ec8ed5c9
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-03-20 11:28:35 +0530

    Time: 0 ms (100%), Space: 42.5 MB (12.85%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);ListNode large = new ListNode(0);
+        ListNode smallP = small, largeP = large;
+        while(head!=null){
+            if(head.val < x){
+                smallP.next = head;
+                smallP = smallP.next;
+            }
+            else{
+                largeP.next = head;
+                largeP = largeP.next;
+            }
+            head = head.next;
+        }
+        largeP.next = null;
+        smallP.next = large.next;
+        return small.next;
+    }
+}
