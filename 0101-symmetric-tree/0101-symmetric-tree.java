961c9a5203d774222f1205f00d5947cbc8887af9
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-03-21 12:32:38 +0530

    Time: 0 ms (100%), Space: 41.8 MB (52.78%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return Symmetric(root.left,root.right);
+    }
+    public boolean Symmetric(TreeNode root1,TreeNode root2){
+        if(root1==null && root2==null) return true;
+        if(root1==null || root2==null) return false;
+        if(root1.val!=root2.val) return false;
+        boolean left = Symmetric(root1.left,root2.right);
+        boolean right = Symmetric(root1.right,root2.left);
+        return left&&right;
+    }
+}
