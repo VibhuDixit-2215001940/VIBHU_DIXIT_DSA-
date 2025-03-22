651e680506cc3ece21b3497adac40685fee16466
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-03-22 11:20:51 +0530

    Time: 0 ms (100%), Space: 42.1 MB (78.91%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,19 +1,13 @@
-class Solution {
+public class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ll = new ArrayList<>();
-        if (root == null) return ll; 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            int n = q.size();
-            TreeNode rv = null;
-            while(n-->0){
-                rv = q.poll();
-                if (rv.left != null) q.add(rv.left);
-                if (rv.right != null) q.add(rv.right);
-            }
-            ll.add(rv.val);
-        }
-        return ll;
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null) return;
+        if(currDepth == result.size()) result.add(curr.val);
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
     }
 }
