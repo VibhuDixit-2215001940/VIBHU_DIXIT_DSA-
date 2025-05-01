3ee103d896c35c436f39074188939fbb3dd293ab
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-05-01 09:08:19 +0530

    Time: 0 ms (100%), Space: 45.9 MB (29.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,12 +1,26 @@
 class Solution {
-    public void setZeroes(int[][] arr) {
-        int n = arr.length, m = arr[0].length;
-        boolean f1=false,f2=false;
-        for(int i=0;i<n;i++) if(arr[i][0]==0) f1=true;
-        for(int i=0;i<m;i++) if(arr[0][i]==0) f2=true;
-        for(int i=1;i<n;i++) for(int j=1;j<m;j++) if(arr[i][j]==0) arr[i][0] = arr[0][j] = 0;
-        for(int i=1;i<n;i++) for(int j=1;j<m;j++) if(arr[i][0]==0||arr[0][j]==0) arr[i][j]=0;
-        if(f1) for(int i=0;i<n;i++) arr[i][0] = 0;
-        if(f2) for(int i=0;i<m;i++) arr[0][i] = 0;
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+
+        // Step 1: Mark rows and columns that need to be zero
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Step 2: Set cells to zero
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 }
