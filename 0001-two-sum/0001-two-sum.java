057d187ab5258916df81d8f203a94042cdb00412
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-05-20 20:59:59 +0530

    Time: 2 ms (98.83%), Space: 45.1 MB (46.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-        Map<Integer, Integer> mp = new HashMap<>();
+        Map<Integer,Integer> mp = new HashMap<>();
         for(int i=0;i<arr.length;i++){
-            int x = target-arr[i];
-            if(mp.containsKey(x)) return new int[]{mp.get(x),i};
+            if(mp.containsKey(target-arr[i])) return new int[]{mp.get(target-arr[i]),i};
             mp.put(arr[i],i);
         }
         return new int[]{};
