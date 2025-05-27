2a5c26941447937d0f55e8429a4cea1542511051
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-05-28 01:27:28 +0530

    Time: 0 ms (100%), Space: 42 MB (68.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,20 +1,20 @@
 class Solution {
     public void sortColors(int[] arr) {
-        int n = arr.length,one=0,zero=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]==1) one++;
-            else if(arr[i]==0) zero++;
-        }
-        for(int i=0;i<n;i++){
-            if(zero>0){
-                arr[i]=0;
-                zero--;
+        int st=0,mid=0,hi=arr.length-1;
+        while(mid<=hi){
+            if(arr[mid]==0){
+                int temp = arr[mid];
+                arr[mid] = arr[st];
+                arr[st] = temp;
+                st++;mid++;
             }
-            else if(one>0){
-                arr[i]=1;
-                one--;
+            else if(arr[mid]==1) mid++;
+            else{
+                int temp = arr[mid];
+                arr[mid] = arr[hi];
+                arr[hi] = temp;
+                hi--;
             }
-            else arr[i]=2;
         }
     }
 }
