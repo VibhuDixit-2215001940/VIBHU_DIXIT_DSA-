34c39b07807e71ad5dde00dcdd26c951b6c2667c
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-05-30 11:53:33 +0530

    Time: 1 ms (99.85%), Space: 52.9 MB (65.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,17 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+    public int majorityElement(int[] arr) {
+        int n = arr.length, majority = arr[0], count = 1;
+        for(int i=1;i<n;i++){
+            int ele = arr[i];
+            if(ele == majority) count++;
+            else{
+                count--;
+                if(count == 0){
+                    count = 1;
+                    majority = ele;
+                }
+            }
+        }
+        return majority;
     }
 }
