8a3d3cf1fcced59df0257ed482c2f3b33c5b94d8
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-06-03 11:22:04 +0530

    Time: 12 ms (5.2%), Space: 49.3 MB (65.27%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -1,17 +1,27 @@
 class Solution {
-    public int maxSubarraySumCircular(int[] nums) {
-      int total=0;
-      int currmin=0;
-      int currmax=0;
-      int max=nums[0];
-      int min=nums[0];
-      for(int num:nums){
-          total+=num;
-          currmax=Math.max(currmax+num,num);
-          currmin=Math.min(currmin+num,num);
-          max=Math.max(currmax,max);
-          min=Math.min(currmin,min);
-      }
-      return max<0?max:Math.max(max,total-min);
+    public int kadanesMin(int[] arr,int n){
+        int sum=arr[0],m=arr[0];
+        for(int i=1;i<n;i++){
+            sum = Math.min(sum+arr[i],arr[i]);
+            m = Math.min(m,sum);
+        }
+        return m;
+    }
+    public int kadanesMax(int[] arr,int n){
+        int sum=arr[0],m=arr[0];
+        for(int i=1;i<n;i++){
+            sum = Math.max(sum+arr[i],arr[i]);
+            m = Math.max(m,sum);
+        }
+        return m;
+    }
+    public int maxSubarraySumCircular(int[] arr) {
+        int n = arr.length;
+        int total = Arrays.stream(arr).sum();
+        int minn = kadanesMin(arr,n);
+        int maxx = kadanesMax(arr,n);
+        int circ = total-minn;
+        if(maxx>0) return Math.max(maxx,circ);
+        return maxx;
     }
 }
