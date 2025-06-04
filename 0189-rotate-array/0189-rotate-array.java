728d26662d6d9a6a8045d7cb7db2cdf03c748297
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-06-04 09:46:55 +0530

    Time: 1 ms (47.17%), Space: 57.2 MB (85.63%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,9 +1,8 @@
 class Solution {
     public void rotate(int[] arr, int k) {
-        int n = arr.length,j=0;k %=n;
-        int[] brr = new int[n];
-        for(int i=n-k;i<n;i++) brr[j++] = arr[i];
-        for(int i=0;i<n-k;i++) brr[j++] = arr[i];
-        System.arraycopy(brr,0,arr,0,n);
+        int n=arr.length;int[] brr = new int[n];int c=0;k%=n;
+        for(int i=n-k;i<n;i++) brr[c++]=arr[i];
+        for(int i=0;i<n-k;i++) brr[c++]=arr[i];
+        for(int i=0;i<n;i++) arr[i]=brr[i];
     }
 }
