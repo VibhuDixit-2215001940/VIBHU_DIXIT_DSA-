b20da0680abddba5d448e06048a47916d349cf18
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-06-24 07:23:56 +0530

    Time: 2 ms (98.58%), Space: 43.5 MB (93.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,7 +1,6 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length(),maxLength = 0,left = 0;
-        int[] charIndex = new int[128];
+        int n = s.length(),maxLength = 0,left = 0;int[] charIndex = new int[128];
         Arrays.fill(charIndex, -1);
         for (int right = 0; right < n; right++) {
             if (charIndex[s.charAt(right)] >= left) left = charIndex[s.charAt(right)] + 1;
@@ -11,5 +10,3 @@ class Solution {
         return maxLength;
     }
 }
-
-
