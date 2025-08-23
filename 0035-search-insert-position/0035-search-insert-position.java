01b4d7b170a06c67aa1c89c8f2e452fea2e4b31b
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-08-23 09:25:06 +0530

    Time: 0 ms (100%), Space: 42.9 MB (66.24%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int l=0,h=arr.length-1;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]<target) l = mid+1;
+            else h = mid-1;
+        }
+        return l;
+    }
+}
